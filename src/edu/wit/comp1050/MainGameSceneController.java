51b7443ac3e22b59fc0609f76d9f2e8500a7d474
danielclapp <71087379+danielclapp@users.noreply.github.com>
2020-11-22 22:26:02 -0500

    ENTER BUTTON!

--- src/edu/wit/comp1050/MainGameSceneController.java
+++ src/edu/wit/comp1050/MainGameSceneController.java
@@ -1,5 +1,6 @@
 package edu.wit.comp1050;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +9,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -178,7 +181,10 @@ public class MainGameSceneController {
         }
 
         //does the same thing as the guess button
-        public void handleEnterKeyTyped () {
+        public void handleEnterKeyTyped (KeyEvent event) throws IOException {
+            if(event.getCode().equals(KeyCode.ENTER)) {
+                handleGuessButtonPressed();
+            }
         }
 
         //creates a hand to be scored
